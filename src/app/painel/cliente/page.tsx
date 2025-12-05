f74229ee1c1e677fb470308fa1ce4ded17c7ad4a
dolarjotaerri-afk <dolarjotaerri@gmail.com>
2025-12-05 13:11:50 -0300

    Atualizar o page.tsx

--- src/app/painel/cliente/page.tsx
+++ src/app/painel/cliente/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { supabase } from "../../../supabaseClient";
+import { supabase } from "../../../lib/supabaseClient";
 
 type ClienteResumo = {
   id: string;
